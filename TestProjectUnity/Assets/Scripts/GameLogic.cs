a6128056f157ee42439068bd5bdcc015ac618785
Oleg Cheban <odcheban@gmail.com>
2018-11-12 20:28:47 +0300

    CreateRectangle

---
+++ TestProjectUnity/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+    private Transform parentRect;
+
+    private void Start()
+    {
+        parentRect = GameObject.Find("Rectangles").transform;
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Transform hit = GetTransformHit();
+            RectangleObj cloneRect = Instantiate(Resources.Load("Rect") as GameObject, GetPositionMouse(), Quaternion.identity, parentRect)
+                .GetComponent<RectangleObj>();
+            cloneRect.CreateRect(this);
+        }
+        else
+        {
+
+        }
+    }
+
+    Vector2 GetPositionMouse()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    Transform GetTransformHit()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(GetPositionMouse(), Vector2.zero);
+        return hit.transform;
+    }
+}
---
+++ TestProjectUnity/Assets/Scripts/RectangleObj.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectangleObj : MonoBehaviour {
+    GameLogic gLogic;
+
+    public void CreateRect(GameLogic gl)
+    {
+        GetComponent<SpriteRenderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+        gLogic = gl;
+    }
+}
